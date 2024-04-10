a79a77b51fdbbcefea3246d8046c486581f8eed0
akankshaajoshi <joshiaj33@gmail.com>
2024-04-10 07:27:18 +0530

    fix try-on css

--- frontend/src/Components/TryOn/TryOn.js
+++ frontend/src/Components/TryOn/TryOn.js
@@ -1,7 +1,13 @@
 const TryOn = () => {
   return (
-    <div style={{ width: "100vw", height: "100vh", margin: "0px", overflow: "hidden" }}>
-      <iframe src="index2.html" width="100%" height="100%" style={{ border: "none" }} title="Try on Site"></iframe>
+    <div style={{ width: "100vw", height: "100vh", margin: "0px", overflow: "hidden", boxSizing: "border-box" }}>
+      <iframe
+        src="index2.html"
+        width="100%"
+        height="100%"
+        style={{ border: "none", boxSizing: "border-box" }}
+        title="Try on Site"
+      ></iframe>
     </div>
   );
 };
